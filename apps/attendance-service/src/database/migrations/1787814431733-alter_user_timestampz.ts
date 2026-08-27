b9deee217ce21b2c511c8e6cbc9ba69892d594ae
arsa-dg <arsaadg891@gmail.com>
2026-08-27 14:09:19 +0700

    fix(user): created_at, updated_at type timestamptz

---
+++ apps/attendance-service/src/database/migrations/1787814431733-alter_user_timestampz.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AlterUserTimestampz1787814431733 implements MigrationInterface {
+    name = 'AlterUserTimestampz1787814431733'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "created_at"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "updated_at"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "updated_at"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "updated_at" TIMESTAMP NOT NULL DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "created_at"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "created_at" TIMESTAMP NOT NULL DEFAULT now()`);
+    }
+
+}
--- apps/attendance-service/src/user/user.entity.ts
+++ apps/attendance-service/src/user/user.entity.ts
@@ -32,9 +32,9 @@ export class User {
   @Column({ default: 'USER' })
   role!: string;
 
-  @CreateDateColumn({ name: 'created_at' })
+  @CreateDateColumn({ type: 'timestamptz', name: 'created_at' })
   createdAt!: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
+  @UpdateDateColumn({ type: 'timestamptz', name: 'updated_at' })
   updatedAt!: Date;
 }
