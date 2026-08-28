a78bed8e2be2018a2c93a6b3e4c3189f21bcfe98
arsa-dg <arsaadg891@gmail.com>
2026-08-28 17:05:31 +0700

    feat(attendance): entity and migration

---
+++ apps/attendance-service/src/attendance/attendance.entity.ts
@@ -0,0 +1,45 @@
+import { User } from '../user/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('attendance')
+export class Attendance {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ name: 'user_id', type: 'uuid' })
+  userId!: string;
+
+  @ManyToOne(
+    () => User,
+    {
+      onDelete: 'CASCADE',
+    },
+  )
+  @JoinColumn({ name: 'user_id' })
+  user!: User;
+
+  @Index()
+  @Column({ name: 'date', type: 'date' })
+  date!: Date;
+
+  @Column({ name: 'clock_in', type: 'timestamptz' })
+  clockIn!: Date;
+
+  @Column({ name: 'clock_out', type: 'timestamptz', nullable: true })
+  clockOut?: Date | null;
+
+  @CreateDateColumn({ type: 'timestamptz', name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', name: 'updated_at' })
+  updatedAt!: Date;
+}
